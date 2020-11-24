05ef4c226897676798e495a722251e42de0249ad
Your Name <you@example.com>
2020-11-24 11:15:45 +0800

    Site updated: 2020-11-24 11:15:32

--- 404/js/script.js
+++ 404/js/script.js
@@ -84,4 +84,4 @@ const nav = document.querySelector('nav');
 burger.addEventListener('click', e => {
   burger.dataset.state === 'closed' ? burger.dataset.state = "open" : burger.dataset.state = "closed";
   nav.dataset.state === "closed" ? nav.dataset.state = "open" : nav.dataset.state = "closed";
-});<script type="text/javascript" src="https://cdn.jsdelivr.net/npm/kity@2.0.4/dist/kity.min.js"></script><script type="text/javascript" src="https://cdn.jsdelivr.net/npm/kityminder-core@1.4.50/dist/kityminder.core.min.js"></script><script defer="true" type="text/javascript" src="https://cdn.jsdelivr.net/npm/hexo-simple-mindmap@0.2.0/dist/mindmap.min.js"></script><link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/hexo-simple-mindmap@0.2.0/dist/mindmap.min.css">
+});
